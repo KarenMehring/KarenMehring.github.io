966996fe1f564f3cebf154c1a07c9f8b5f8a81af
KarenMehring <117677134+KarenMehring@users.noreply.github.com>
2022-11-07 16:07:54 -0300

    Add files via upload

---
+++ Alojamiento.js
@@ -0,0 +1,437 @@
+//Carousel 1
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores',
+      arrows: {
+          prev: '.carousel-anterior',
+          next: '.carousel-siguiente',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 2
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista2'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores2',
+      arrows: {
+          prev: '.carousel-anterior2',
+          next: '.carousel-siguiente2',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 3
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista3'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores3',
+      arrows: {
+          prev: '.carousel-anterior3',
+          next: '.carousel-siguiente3',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 4
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista4'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores4',
+      arrows: {
+          prev: '.carousel-anterior4',
+          next: '.carousel-siguiente4',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 5
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista5'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores5',
+      arrows: {
+          prev: '.carousel-anterior5',
+          next: '.carousel-siguiente5',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel6
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista6'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores6',
+      arrows: {
+          prev: '.carousel-anterior6',
+          next: '.carousel-siguiente6',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 7
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista7'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores7',
+      arrows: {
+          prev: '.carousel-anterior7',
+          next: '.carousel-siguiente7',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 8
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista8'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores8',
+      arrows: {
+          prev: '.carousel-anterior8',
+          next: '.carousel-siguiente8',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 9
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista9'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores9',
+      arrows: {
+          prev: '.carousel-anterior9',
+          next: '.carousel-siguiente9',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 10
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista10'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores10',
+      arrows: {
+          prev: '.carousel-anterior10',
+          next: '.carousel-siguiente10',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 11
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista11'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores11',
+      arrows: {
+          prev: '.carousel-anterior11',
+          next: '.carousel-siguiente11',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 12
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista12'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores12',
+      arrows: {
+          prev: '.carousel-anterior12',
+          next: '.carousel-siguiente12',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 13
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista13'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores13',
+      arrows: {
+          prev: '.carousel-anterior13',
+          next: '.carousel-siguiente13',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+//Carousel 14
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista14'),{
+      slidesToShow: 4,
+      slidesToScroll: 4, 
+      dots: '.carousel-indicadores14',
+      arrows: {
+          prev: '.carousel-anterior14',
+          next: '.carousel-siguiente14',
+      },
+      responsive: [
+          {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },{
+            // screens greater than >= 1024px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          }
+      ]
+  })
+})
+
+
+
+
